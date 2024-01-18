84cc0abd25c390cc66c8c71ce237cb9a6a92b706
[Ryad] <[ryadzaatour@gmail.com]>
2024-01-18 17:06:19 +0100

    link ok

--- src/Controller/DailyController.php
+++ src/Controller/DailyController.php
@@ -6,11 +6,13 @@ use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
-class ProfileController extends AbstractController
+class DailyController extends AbstractController
 {
-    #[Route('/profile', name: 'app_profile')]
+    #[Route('/daily', name: 'app_daily')]
     public function index(): Response
     {
-        return $this->render('profile/index.html.twig');
+        return $this->render('daily/index.html.twig', [
+            'controller_name' => 'DailyController',
+        ]);
     }
 }
